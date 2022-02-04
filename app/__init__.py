4923810128346353a099db33490fc8fba496e044
james-muriithi <muriithijames556@gmail.com>
2022-02-04 17:53:38 +0300

    added auth module

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from .main import main as main_blueprint
+from .auth import auth as auth_blueprint
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -16,6 +17,7 @@ def create_app(config_name):
 
     # register blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
